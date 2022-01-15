ec5c11eda1b6bef6a744c11fcfba886607a7c5cd
liu yi <3113929638@qq.com>
2022-01-16 07:47:12 +0800

    2022.1.16

--- OJ/luogu/P5677.cpp
+++ OJ/luogu/P5677.cpp
@@ -4,13 +4,35 @@ using namespace std;
 
 const int MAXN = 1e5;
 
-int n;
+class BitTree {
+private:
+  int a[MAXN];
+  static int lowbit(int x) {
+    return x & -x;
+  }
+
+public:
+  void add(int x, int y) {
+    for (; x < MAXN; x += lowbit(x)) {
+      a[x] += y;
+    }
+  }
+};
+
+struct que {
+  int x, y;
+}que[MAXN];
+
+int n, m;
 int a[MAXN];
 
 int main() {
-  cin >> n;
+  cin >> n >> m;
   for (int i = 1; i <= n; i++) {
     cin >> a[i];
+  }
+  for (int i = 1; i <= m; i++) {
+    cin >> que[i].x >> que[i].y;
   }
   return 0;
 }
