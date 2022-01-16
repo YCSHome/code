9c80e8d7df63b4b6b1d7a40844dfdcba3a6804e8
liu yi <3113929638@qq.com>
2022-01-16 13:50:41 +0800

    new

--- OJ/luogu/P3398.cpp
+++ OJ/luogu/P3398.cpp
@@ -2,7 +2,47 @@
 
 using namespace std;
 
-int main(){
-  int a;
-  cin >> a;
+const int MAXN = 1e6;
+
+struct edge {
+  int v, next;
+}e[MAXN];
+
+int head[MAXN];
+int tot = 0;
+int n, q;
+
+void add(int u, int v) {
+  e[++tot] = {v, head[u]};
+  head[u] = tot;
+}
+
+namespace LCA {
+  int father[MAXN][30];
+  int d[MAXN];
+  bool vis[MAXN];
+
+  void init(int u, int fa) {
+    if (vis[u]) return;
+    vis[u] = true;
+    for (int i = head[u]; i; i = e[i].next) {
+      int v = e[i].v;
+      init(v, fa);
+    }
+  }
+
+  void init() {
+    memset(vis, false, sizeof(vis));
+  }
+};
+
+int main() {
+  cin >> n >> q;
+  for (int i = 1; i <= n - 1; i++) {
+    int u, v;
+    cin >> u >> v;
+    add(u, v);
+    add(v, u);
+  }
+  return 0;
 }
