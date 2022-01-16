9b6e87b36fe7a8e886fc87f3e6002bf70707655a
liu yi <3113929638@qq.com>
2022-01-16 16:56:02 +0800

    new

--- OJ/luogu/P1972.cpp
+++ OJ/luogu/P1972.cpp
@@ -2,7 +2,14 @@
 
 using namespace std;
 
-const int MAXN = 1e6;
+const int MAXN = 1e7;
+
+struct que {
+  int x, y, id;
+  bool operator<(const que& cmp) const {
+    return y < cmp.y;
+  }
+}qu[MAXN];
 
 class BitTree {
 private:
@@ -13,27 +20,51 @@ private:
 
 public:
   void add(int x, int k) {
-    for (; x < MAXN; x += lowbit(x)) {
+    for (; x < MAXN - 1; x += lowbit(x)) {
       a[x] += k;
     }
   }
 
   int get(int x) {
     int ans = 0;
-    for (; x; x -= lowbit(x)) {
+    for (; x > 0; x -= lowbit(x)) {
       ans += a[x];
     }
     return ans;
   }
-};
+}tree;
 
 int a[MAXN];
+int d[MAXN];
+int ans[MAXN];
 
 int main() {
   int n;
   cin >> n;
   for (int i = 1; i <= n; i++) {
     cin >> a[i];
+  }
+  int q;
+  cin >> q;
+  for (int i = 1; i <= q; i++) {
+    cin >> qu[i].x >> qu[i].y;
+    qu[i].id = i;
+  }
+  sort(qu + 1, qu + 1 + q);
+  int next = 1;
+  for (int i = 1; i <= q; i++) {
+    for (int j = next; j <= qu[i].y; j++) {
+      if (d[a[j]]) {
+        tree.add(d[a[j]], -1);
+      }
+      tree.add(j, 1);
+      d[a[j]] = j;
+    }
+    next = qu[i].y + 1;
+    ans[qu[i].id] = tree.get(qu[i].y) - tree.get(qu[i].x - 1);
+  }
+  for (int i = 1; i <= q; i++) {
+    cout << ans[i] << endl;
   }
   return 0;
 }
--- OJ/luogu/P5677.cpp
+++ OJ/luogu/P5677.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-const int MAXN = 1e5;
+const int MAXN = 1e6;
 
 class BitTree {
 private:
@@ -17,22 +17,88 @@ public:
       a[x] += y;
     }
   }
-};
+
+  int get(int x) {
+    int ans = 0;
+    for (; x; x -= lowbit(x)) {
+      ans += a[x];
+    }
+    return ans;
+  }
+}tree;
 
 struct que {
-  int x, y;
+  int l, r, id;
+  bool operator<(const que& cmp) const {
+    if (r != cmp.r) return r < cmp.r;
+    return l < cmp.l;
+  }
 }que[MAXN];
 
+struct node {
+  int l, r;
+  bool operator<(const node& cmp) const {
+    if (r != cmp.r) return r < cmp.r;
+    return l < cmp.l;
+  }
+}nodes[MAXN];
+
+struct num {
+  int value, id;
+  bool operator<(const num& cmp) const {
+    return value < cmp.value;
+  };
+}a[MAXN];
+
+int tot = 0;
 int n, m;
-int a[MAXN];
+int b[MAXN];
+
+void add_node(const num& x, const num& y) {
+  int i = min(x.id, y.id);
+  int j = max(x.id, y.id);
+  nodes[++tot] = {i, j};
+}
 
 int main() {
   cin >> n >> m;
+  if (n == 1) {
+    cout << 0;
+    return 0;
+  }
   for (int i = 1; i <= n; i++) {
-    cin >> a[i];
+    cin >> a[i].value;
+    a[i].id = i;
   }
   for (int i = 1; i <= m; i++) {
-    cin >> que[i].x >> que[i].y;
+    cin >> que[i].l >> que[i].r;
+    que[i].id = i;
   }
+  sort(que + 1, que + 1 + m);
+  sort(a + 1, a + 1 + n);
+  add_node(a[1], a[2]);
+  add_node(a[n - 1], a[n]);
+  for (int i = 2; i < n; i++) {
+    int l = a[i].value - a[i - 1].value;
+    int r = a[i + 1].value - a[i].value;
+    if (l < r) {
+      add_node(a[i], a[i - 1]);
+    } else if (l > r) {
+      add_node(a[i], a[i + 1]);
+    } else {
+      add_node(a[i], a[i - 1]);
+      add_node(a[i], a[i + 1]);
+    }
+  }
+  sort(nodes + 1, nodes + 1 + tot);
+  long long ans = 0;
+  for (int i = 1, j = 1; i <= m; i++) {
+    while (nodes[j].r <= que[i].r && j <= tot) {
+      tree.add(nodes[j].l, 1);
+      j++;
+    }
+    ans += (long long)que[i].id * (j - 1 - tree.get(que[i].l - 1));
+  }
+  cout << ans << endl;
   return 0;
 }
